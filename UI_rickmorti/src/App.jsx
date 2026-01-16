de975fd92ae48dfbcb42b80291443bf788ae9a0e
gaetan-pardon <gaetan.pardon@isen.yncrea.fr>
2026-01-16 11:36:34 +0100

    shearch option menu test

--- UI_rickmorti/src/App.jsx
+++ UI_rickmorti/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect , useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -21,30 +21,59 @@ async function testApi() {
 testApi(); */
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0) ;
   const [items, setItems] = useState([]);      // ce que tu affiches
   const [loading, setLoading] = useState(true); // état de chargement
   const [error, setError] = useState(null);     // message d’erreur
+  const [input, setInput] = useState("");
+  const [filter, setFilter] = useState(""); 
+  const [itemsfilter, setItemsfilter] = useState([]); 
 
-  useEffect(() => { 
+  useEffect(() => {
     async function load() {
-  try {
-    const res = await fetch("https://rickandmortyapi.com/api/character");
+      try {
+        const res = await fetch("https://rickandmortyapi.com/api/character");
 
-    if (!res.ok) {
-      throw new Error(res.status); //HTTP ${ }
-    }
+        if (!res.ok) {
+          throw new Error(res.status); //HTTP ${ }
+        }
 
-    const data = await res.json();
-    console.log("Données reçues :", data);
-    setItems(data);
-  } catch (err) {
-    console.log("Erreur :", err.message);
-  }finally {
+        const data = await res.json();
+        console.log("Données reçues :", data);
+        setItems(data);
+      } catch (err) {
+        console.log("Erreur :", err.message);
+      } finally {
         setLoading(false);
       }
     } load();
-}, []); 
+  }, []);
+
+  useEffect(() => {
+  async function loadwf() {
+    try {
+      setLoading(true);
+      setError(null);
+
+      
+      const res = await fetch("https://rickandmortyapi.com/api/character/?" + input);
+      if (!res.ok) throw new Error(res.status);
+
+      const data = await res.json();
+      console.log("Données reçues :", data);
+      setItemsfilter(data);
+    } catch (e) {
+      setError(e.message);
+      setItems([]);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  loadwf();
+}, []);
+
+
 
   if (loading) return <p>Chargement...</p>;
   if (error) return <p>Erreur: {error}</p>;
@@ -72,17 +101,30 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR <br />
           {items.results[count].name} items.
         </p>
-            <div key={items.results[count].id}>
-              <h2>{items.results[count].id} {items.results[count].name}</h2>
-              <img src={items.results[count].image} alt={items.results[count].name} />
-            </div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setFilter(input.trim());
+          }}
+        >
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Tape un filtre (ex: rick)"
+          />
+          <button type="submit">Rechercher</button>
+        </form>
+        <div key={items.results[count].id}>
+          <h2>{items.results[count].id} {items.results[count].name}</h2>
+          <img src={items.results[count].image} alt={items.results[count].name} />
+        </div>
 
         {items.results.map((item) => (
-            <div key={item.id}>
-              <h2>{item.id} {item.name}</h2>
-              <img src={item.image} alt={item.name} />
-            </div>
-          ))}
+          <div key={item.id}>
+            <h2>{item.id} {item.name}</h2>
+            <img src={item.image} alt={item.name} />
+          </div>
+        ))}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
